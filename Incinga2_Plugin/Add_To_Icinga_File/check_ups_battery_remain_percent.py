9f8c59e1397d753d74f4ff894cf3202987e635ea
j13tw <j13tw@yahoo.com.tw>
2018-03-19 11:51:00 +0800

    fix file name error & add battery level status test

---
+++ Incinga2_Plugin/Add_To_Icinga_File/check_ups_battery_remain_percent.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import requests
+import json
+import os, sys
+import socket
+
+
+hostname = '127.0.0.1'					#chang to your service IP
+port = '5000'							#chang to your service Port
+
+localOS = os.system('uname 2>&1 >/var/tmp/os.txt')
+if(localOS == 0):
+	response = os.system('ping -c 1 ' + hostname + ' 2>&1 >/var/tmp/ping.txt')
+#	os.system('clear')
+else:
+	response = os.system('ping -n 1 ' + hostname + ' 2>&1 >ping.txt')
+#	os.system('cls')
+
+if response == 0:						# check network sevice & server is on
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	result = sock.connect_ex((hostname, int(port)))
+	if result == 0:
+		sock.close()
+		distance = 'http://' + hostname + ':' + port
+		r = requests.get(distance)
+		value = r.content.decode('utf-8')	# get return json value
+		key = json.loads(value)
+		outputStatus = key['battery'][0]['status'][0]
+		outputPersent = outputStatus['batteryRemain_percent']
+		if(int(outputPersent)) >= 30:
+			print ("Battery Remain Percent : "+ batteryRemain_percent + " %| Battery Level=" + batteryRemain_percent + "%;50;30")
+			sys.exit(0)
+		else:
+			print ("Battery Remain Percent : "+ batteryRemain_percent + " % (Quick Close All Service !)" + "| Battery Level=" + batteryRemain_percent + "%;50;30")
+			sys.exit(3)
+	else:
+	   	print ('http://' + hostname +':' + port + ' Service Port Found !')
+	   	sys.exit(2)   
+else:
+  	print ('http://', hostname, ' Server IP Not Found !')
+  	sys.exit(2)
